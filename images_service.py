8188a163f0e59802df9be24a344fe9eb5ed3a6d3
Omkar Kaul <nzomkar@gmail.com>
2021-09-08 20:02:43 +1200

    Refactor url validation into two separate methods

--- images_service.py
+++ images_service.py
@@ -4,16 +4,25 @@ from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin, urlparse
 
 class ImageService:
-    def _is_valid(self, url):
+    def _is_valid_url(self, url):
         """ VALIDATE URL """
+        try:
+            parsed_url = urlparse(url)
+            return bool(parsed_url.netloc) and bool(parsed_url.scheme)
+        except:
+            return False
+
+    def _is_valid_image_url(self, url):
+        """ VALIDATE IMAGE URL """
         valid_image_extensions = {"jpg", "png", "gif", "jpeg"}
         extension = url[url.rfind('.')+1:]
 
         parsed_url = urlparse(url)
+
         if extension not in valid_image_extensions:
             return False
 
-        return bool(parsed_url.netloc) and bool(parsed_url.scheme)
+        return self._is_valid_url(url)
 
     def _update_url_list(self, url, url_list, soup, tag, attribute):
         """ RUN THROUGH DOM TO FIND IMAGE URLS AND APPEND TO URL LIST BY REFERENCE """
@@ -30,7 +39,7 @@ class ImageService:
             except ValueError:
                 pass
 
-            if self._is_valid(absolute_image_url):
+            if self._is_valid_image_url(absolute_image_url):
                 url_list.append(absolute_image_url)
 
     def get_all(self, request):
@@ -38,7 +47,10 @@ class ImageService:
         url = request.args.get("url")
         if not url:
             return {"message": "Missing URL from request"}, 400
-        
+
+        if not self._is_valid_url(url):
+            return {"message": "Bad URL submitted"}, 400
+
         response = r.get(url)
         soup = bs(response.content, "html.parser")
 
